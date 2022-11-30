8c592c69ab82e7973811fac060aab09be1cf2f34
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2022-11-30 20:57:55 +0600

    comment on guest layout about js and css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,18 +11,26 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    @vite(['resources/assets/template/plugins/fontawesome-free/css/all.min.css', 'resources/assets/template/plugins/icheck-bootstrap/icheck-bootstrap.min.css', 'resources/assets/template/dist/css/adminlte.min.css'])
+    @vite([
+        'resources/assets/template/plugins/fontawesome-free/css/all.min.css', // font awesome
+        'resources/assets/template/plugins/icheck-bootstrap/icheck-bootstrap.min.css', // icheck
+        'resources/assets/template/dist/css/adminlte.min.css', // admin lte
+    ])
 </head>
 
 <body @class([
     'hold-transition',
     'login-page' => Route::is('login'),
     'register-page' => Route::is('register'),
-    ])>
+])>
 
     {{ $slot }}
 
-    @vite(['resources/assets/template/plugins/jquery/jquery.min.js', 'resources/assets/template/plugins/bootstrap/js/bootstrap.bundle.min.js', 'resources/assets/template/dist/js/adminlte.min.js'])
+    @vite([
+        'resources/assets/template/plugins/jquery/jquery.min.js', // jqery
+        'resources/assets/template/plugins/bootstrap/js/bootstrap.bundle.min.js', // bootstrap bundle
+        'resources/assets/template/dist/js/adminlte.min.js', // admin lte
+    ])
 </body>
 
 </html>
